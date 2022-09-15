021e2e01a7776819cf55fe0f9bf7e891c9980c50
rahulhasan31 <rahulhasan666@gmail.com>
2022-09-15 09:51:06 +0600

    add

--- script.js
+++ script.js
@@ -67,7 +67,7 @@ const gameOver = () => {
   // the current time is the finish time
   // so total time taken is current time - start time
   const finishTime = new Date().getTime();
-  const timeTaken = (finishTime - startTime) / 1000;
+  const timeTaken = (finishTime - startTime) / 1000|0;
 
   // show result modal
   resultModal.innerHTML = "";
@@ -132,7 +132,7 @@ displayHistory();
 // Show typing time spent
 setInterval(() => {
   const currentTime = new Date().getTime();
-  const timeSpent = (currentTime - startTime) / 1000;
+  const timeSpent = (currentTime - startTime) / 1000|0;
 
 
   document.getElementById("show-time").innerHTML = `${startTime ? timeSpent : 0} seconds`;
